ea0144e0202394a839fef0c22591e5f4271c6c68
Javi <jiva9423@gmail.com>
2021-03-26 17:02:42 -0400

    Added the 6 app pages. Login/Create Account/Product/Feed/Settings/Chat

--- app/src/main/java/com/example/swipeandshop/ChatPage.java
+++ app/src/main/java/com/example/swipeandshop/ChatPage.java
@@ -2,7 +2,10 @@ package com.example.swipeandshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChatPage extends AppCompatActivity {
 
@@ -11,4 +14,6 @@ public class ChatPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
     }
+
+
 }
--- app/src/main/java/com/example/swipeandshop/FeedPage.java
+++ app/src/main/java/com/example/swipeandshop/FeedPage.java
@@ -2,7 +2,11 @@ package com.example.swipeandshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class FeedPage extends AppCompatActivity {
 
