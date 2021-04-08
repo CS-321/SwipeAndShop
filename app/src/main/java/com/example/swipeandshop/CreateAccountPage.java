c9d8381848b08b9a7f351090fe43bb807f1a3691
Yasmin Ibrahim <yasmin.m.ibrahim@hotmail.com>
2021-04-08 19:46:37 -0400

    Connected to Firebase

--- app/src/main/java/com/example/swipeandshop/CreateAccountPage.java
+++ app/src/main/java/com/example/swipeandshop/CreateAccountPage.java
@@ -1,12 +1,21 @@
 package com.example.swipeandshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+// import com.google.firebase.auth.
 
 public class CreateAccountPage extends AppCompatActivity {
 
@@ -27,7 +36,7 @@ public class CreateAccountPage extends AppCompatActivity {
         signUpButton = findViewById(R.id.button);
         gotoLogin = findViewById(R.id.textView6);
 
-
+        firebaseAuth = FirebaseAuth.getInstance();
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +50,26 @@ public class CreateAccountPage extends AppCompatActivity {
 
                 //validate data
                 validate(fullName, email, phone, password);
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(), ProductPage.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+            }
+        });
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
             }
         });
     }
