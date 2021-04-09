2367746f23c4bdf6866adacffd433b0802f3f70c
Yasmin Ibrahim <yasmin.m.ibrahim@hotmail.com>
2021-04-08 20:05:11 -0400

    Added button to navigate to register page

--- app/src/main/java/com/example/swipeandshop/MainActivity.java
+++ app/src/main/java/com/example/swipeandshop/MainActivity.java
@@ -9,10 +9,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button gotoRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gotoRegister = findViewById(R.id.button2);
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CreateAccountPage.class));
+            }
+        });
     }
 
 }
