939a36e87d21d21740c15cba7b5b6f6395a13431
Javi <jiva9423@gmail.com>
2021-04-08 21:46:00 -0400

    Added Gridlayout in ProductPage

--- app/src/main/java/com/example/swipeandshop/MainActivity.java
+++ app/src/main/java/com/example/swipeandshop/MainActivity.java
@@ -8,11 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenProductPage();
+            }
+        });
+    }
+
+    public void OpenProductPage(){
+        Intent intent = new Intent(this, ProductPage.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/swipeandshop/ProductPage.java
+++ app/src/main/java/com/example/swipeandshop/ProductPage.java
@@ -1,17 +1,48 @@
 package com.example.swipeandshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductPage extends AppCompatActivity {
+    RecyclerView dataList;
+    List<String> titles;
+    List<Integer> images;
+    ProductAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_page);
+        dataList = findViewById(R.id.dataList);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        titles.add("First Item");
+        titles.add("Second Item");
+
+        images.add(R.drawable.ic_airline_seat_flat_angled_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        //images.add(R.drawable.ic_brightness_1_black_24dp);
+        //images.add(R.drawable.ic_build_black_24dp);
+        //images.add(R.drawable.ic_airline_seat_flat_angled_black_24dp);
+        //images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        //images.add(R.drawable.ic_brightness_1_black_24dp);
+        //images.add(R.drawable.ic_build_black_24dp);
+
+        adapter = new ProductAdapter(this,titles,images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
+
+
     }
 }
