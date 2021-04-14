e1dbe2f001e001b689db042b77ad3ffa47f3e0d5
Javi <jiva9423@gmail.com>
2021-04-14 15:40:14 -0400

    Added custom_product_layout.xml for product card. Added Product Adapter.

--- app/src/main/java/com/example/swipeandshop/ProductPage.java
+++ app/src/main/java/com/example/swipeandshop/ProductPage.java
@@ -1,5 +1,6 @@
 package com.example.swipeandshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,22 +28,36 @@ public class ProductPage extends AppCompatActivity {
 
         titles.add("First Item");
         titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
+        titles.add("Second Item");
 
-        images.add(R.drawable.ic_airline_seat_flat_angled_black_24dp);
+
+
+
+        images.add(R.drawable.ic_launcher_background);
         images.add(R.drawable.ic_airplanemode_active_black_24dp);
-        //images.add(R.drawable.ic_brightness_1_black_24dp);
-        //images.add(R.drawable.ic_build_black_24dp);
-        //images.add(R.drawable.ic_airline_seat_flat_angled_black_24dp);
-        //images.add(R.drawable.ic_airplanemode_active_black_24dp);
-        //images.add(R.drawable.ic_brightness_1_black_24dp);
-        //images.add(R.drawable.ic_build_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+        images.add(R.drawable.ic_airplanemode_active_black_24dp);
+
 
         adapter = new ProductAdapter(this,titles,images);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
         dataList.setLayoutManager(gridLayoutManager);
         dataList.setAdapter(adapter);
-
-
     }
 }
