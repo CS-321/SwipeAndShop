a431f250b46bd1a3c7a2c8a89ec5549690792cce
Yasmin Ibrahim <yasmin.m.ibrahim@hotmail.com>
2021-04-21 19:29:11 -0400

    Added verify password functionality and realtime database updates

--- app/src/main/java/com/example/swipeandshop/CreateAccountPage.java
+++ app/src/main/java/com/example/swipeandshop/CreateAccountPage.java
@@ -17,30 +17,40 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 // import com.google.firebase.auth.
 
 public class CreateAccountPage extends AppCompatActivity {
 
-    EditText registerFullName, registerEmail, registerPhone, registerPassword;
+    EditText registerFullName, registerEmail, registerPhone, registerPassword, registerVerifyPassword;
     Button signUpButton;
     TextView gotoLogin;
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_page);
 
-        registerFullName = findViewById(R.id.editTextTextPersonName);
+        registerFullName = findViewById(R.id.fullName);
         registerEmail = findViewById(R.id.editTextTextEmailAddress2);
         registerPhone = findViewById(R.id.editTextPhone);
         registerPassword = findViewById(R.id.editTextTextPassword);
+        registerVerifyPassword = findViewById(R.id.verifyPassword);
         signUpButton = findViewById(R.id.button);
         gotoLogin = findViewById(R.id.textView6);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +61,7 @@ public class CreateAccountPage extends AppCompatActivity {
                 String email = registerEmail.getText().toString();
                 String phone = registerPhone.getText().toString();
                 String password = registerPassword.getText().toString();
+                String verifyPassword = registerVerifyPassword.getText().toString();
 
                 //validate data
 
@@ -75,18 +86,27 @@ public class CreateAccountPage extends AppCompatActivity {
                     registerPassword.setError("Password needs to be at least 6 characters long");
                 }
 
+                if(!password.equals(verifyPassword)) {
+                    registerVerifyPassword.setError("Passwords don't match");
+                }
+
                 firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
+                        user = firebaseAuth.getCurrentUser();
+                        myRef.child("users").child(user.getUid()).child("name").setValue(fullName);
+                        myRef.child("users").child(user.getUid()).child("phone number").setValue(phone);
                         startActivity(new Intent(getApplicationContext(), ProductPage.class));
                         finish();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-
+                        registerEmail.setError("Email already in use");
                     }
                 });
+
+
             }
         });
 
