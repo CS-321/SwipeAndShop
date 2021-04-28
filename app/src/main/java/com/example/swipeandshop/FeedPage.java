19fd6904912698af2d6efb1845ff8b7176523658
Javi <jiva9423@gmail.com>
2021-04-28 14:48:23 -0400

    Images for each prouct are shown in ProductPage

--- app/src/main/java/com/example/swipeandshop/FeedPage.java
+++ app/src/main/java/com/example/swipeandshop/FeedPage.java
@@ -1,5 +1,6 @@
 package com.example.swipeandshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,61 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DiffUtil;
+
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FeedPage extends AppCompatActivity {
+    private static final String TAG = "FeedPage";
+    private CardStackLayoutManager manager;
+    private ProductCardStackAdapter adapter;
+
+    // Firebase references.
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
+    DatabaseReference allProductRef;
+    FirebaseUser user;
+    List<Product> products;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_page);
+        //Database
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        allProductRef = database.getReference().child("products");
+        products = new ArrayList<>();
+
+        //Loads in data to adapter from firebase.
+        loadData();
     }
 
     @Override
@@ -22,4 +71,119 @@ public class FeedPage extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private void paginate() {
+        List<Product> old = adapter.getItems();
+        List<Product> newer = new ArrayList<>(addList());
+        ProductCardStackCallback callback = new ProductCardStackCallback(old, newer);
+        DiffUtil.DiffResult result = DiffUtil.calculateDiff(callback);
+        adapter.setItems(newer);
+        result.dispatchUpdatesTo(adapter);
+    }
+
+    private List<Product> addList() {
+        //products.add(new Product("Item 1", "This is a desc.", "the seller", 10.5f, "https://firebasestorage.googleapis.com/v0/b/swipeandshop-8168d.appspot.com/o/1619581076067.png?alt=media&token=adaeed50-aab3-4f38-a4b1-e27346bf52cd","myid"));
+        //products.add(new Product("Item 1", "This is a desc.", "the seller", 10.5f, "https://i.imgur.com/UYrdDFI.jpeg","myid"));
+        return products;
+    }
+
+    /**Retrieves data from firebase when page is first loaded.*/
+    public void loadData(){
+        //Get viewed products by a person so they dont view them again.
+
+        //Also randomize the data order.
+        // this will load data from firebase and put it into the products list.
+        allProductRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot item_snapshot:snapshot.getChildren()){
+                    System.out.println(item_snapshot.getValue().toString());
+                    Product tempProduct = item_snapshot.getValue(Product.class);
+                    products.add(tempProduct);
+                }
+                setAdapterInfo();
+            }
+
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    /**Sets adapter info for the swiping cards.*/
+    private void setAdapterInfo(){
+        CardStackView cardStackView = findViewById(R.id.card_stacked_view);
+        manager = new CardStackLayoutManager(FeedPage.this, new CardStackListener() {
+            @Override
+            public void onCardDragging(Direction direction, float ratio) {
+                Log.d(TAG, "onCardDragging: d=" + direction.name() + " ratio=" + ratio);
+            }
+
+            @Override
+            public void onCardSwiped(Direction direction) {
+                Log.d(TAG, "onCardSwiped: p=" + manager.getTopPosition() + " d=" + direction);
+                if (direction == Direction.Right){
+                    Toast.makeText(FeedPage.this, "Direction Right", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Top){
+                    Toast.makeText(FeedPage.this, "Direction Top", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Left){
+                    Toast.makeText(FeedPage.this, "Direction Left", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Bottom){
+                    Toast.makeText(FeedPage.this, "Direction Bottom", Toast.LENGTH_SHORT).show();
+                }
+
+                // Paginating
+                if (manager.getTopPosition() == adapter.getItemCount() - 5){
+                    paginate();
+                }
+
+            }
+
+            @Override
+            public void onCardRewound() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardCanceled() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardAppeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", nama: " + tv.getText());
+            }
+
+            @Override
+            public void onCardDisappeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", nama: " + tv.getText());
+            }
+        });
+        manager.setStackFrom(StackFrom.None);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.FREEDOM);
+        manager.setCanScrollHorizontal(true);
+        manager.setSwipeableMethod(SwipeableMethod.Manual);
+        manager.setOverlayInterpolator(new LinearInterpolator());
+        adapter = new ProductCardStackAdapter(addList());
+        System.out.println(products.size() + " my siize 2");
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+        cardStackView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+
 }
--- app/src/main/java/com/example/swipeandshop/ProductPage.java
+++ app/src/main/java/com/example/swipeandshop/ProductPage.java
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Debug;
 import android.text.Layout;
@@ -17,13 +19,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -32,6 +38,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,20 +56,31 @@ public class ProductPage extends AppCompatActivity {
     //Product page objects.
     Activity productActivity;
     CardView createProductView;
+
     int currentLocation = -1;
+    String currentUrl = "";
 
     // Buttons
     Button addProductBtn;
     Button cancelProductBtn;
     Button saveProductBtn;
+    Button addImageBtn;
 
     // Firebase references.
     FirebaseAuth firebaseAuth;
     FirebaseDatabase database;
+    FirebaseStorage storage;
+    StorageReference storageRef;
     FirebaseRecyclerOptions<Product> options;
     FirebaseRecyclerAdapter<Product, ProductViewHolder> myAdapter;
     DatabaseReference myRef;
+    DatabaseReference allProductRef;
     FirebaseUser user;
+
+    //Image info
+    private Uri imageUri;
+    private ImageView imageView;
+
 
 
     @Override
@@ -87,15 +109,23 @@ public class ProductPage extends AppCompatActivity {
         // Product page objects.
         productCardList = findViewById(R.id.dataList); //holds all the users products
         addProductBtn = findViewById(R.id.addProductBtn);
+        addImageBtn = findViewById(R.id.addImageBtn);
         productActivity = (Activity)this;
         products = new ArrayList<>();
-
 
         //Database
         firebaseAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
         user = firebaseAuth.getCurrentUser();
+        //Gets reference to db area with only users products
         myRef = database.getReference().child("users").child(user.getUid()).child("products");
+        //Gets reference to db area where all products are stored.
+        allProductRef = database.getReference().child("products");
+
+        //Firebase Storage
+        storageRef = storage.getReference();
+        imageView = findViewById(R.id.imageView);
 
         // Create Product View Fields
         cancelProductBtn = findViewById(R.id.cancelButton);
@@ -153,11 +183,6 @@ public class ProductPage extends AppCompatActivity {
         EditText name = createProductView.findViewById(R.id.nameInput);
         EditText desc = createProductView.findViewById(R.id.descriptionInput); //LET IN
 
-
-         // updating the grid view to include the new product
-        createProductView.setVisibility(View.GONE);
-        addProductBtn.setVisibility(View.VISIBLE);
-
         //Add product to Realtime database
         user = firebaseAuth.getCurrentUser();
 
@@ -165,17 +190,34 @@ public class ProductPage extends AppCompatActivity {
         // if currentLocation < 0 then create a new object.
         if(currentLocation >= 0){
             product = products.remove(currentLocation);
-            product = new Product(name.getText().toString(),desc.getText().toString(), "Javi", 10.90f, R.drawable.ic_launcher_background,product.productId);
+            if(currentUrl.length() == 0){
+                currentUrl = product.getImageUrl();
+            }
+            product = new Product(name.getText().toString(),desc.getText().toString(), "Javi", 10.90f, currentUrl,product.productId);
             products.add(0,product);
             myRef.child(product.productId).setValue(product);
+            allProductRef.child(product.productId).setValue(product);
         }else{
+            if(currentUrl.length() == 0){
+                Toast.makeText(ProductPage.this, "You need to add at least one image.", Toast.LENGTH_SHORT).show();
+                return;
+            }
             DatabaseReference newRef = myRef.push();
-            product = new Product(name.getText().toString(),desc.getText().toString(), "Javi", 10.90f, R.drawable.ic_launcher_background,newRef.getKey());
+            product = new Product(name.getText().toString(),desc.getText().toString(), "Javi", 10.90f, currentUrl,newRef.getKey());
             products.add(product); // add our new product to the list
             newRef.setValue(product);
+            allProductRef.child(product.productId).setValue(product);
         }
 
+        //This closes the editing/creation of the product.
+        createProductView.setVisibility(View.GONE);
+        addProductBtn.setVisibility(View.VISIBLE);
+
+        //sets default image (will be changed to show current product image)
+        imageView.setImageResource(R.drawable.googleg_disabled_color_18);
         this.currentLocation = -1;
+        this.currentUrl = "";
+        // updating the grid view to include the new product
         productCardList.setAdapter(myAdapter);
     }
 
@@ -200,7 +242,7 @@ public class ProductPage extends AppCompatActivity {
         });
     }
 
-    /**Loads data into the firebaseUI adapter and displays it in the grid layout.*/
+    /**Loads data into teh firebaseUI adapter and displays it in the grid layout.*/
     public void showData(){
         // This takes the loaded data from firebase and puts it into the adapter.
         myAdapter = new FirebaseRecyclerAdapter<Product, ProductViewHolder>(options) {
@@ -208,6 +250,12 @@ public class ProductPage extends AppCompatActivity {
             protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Product product) {
                 productViewHolder.title.setText(product.getName());
                 //set other info here.
+                Picasso.get()
+                        .load(product.getImageUrl())
+                        .fit()
+                        .centerCrop()
+                        .into(productViewHolder.gridIcon);
+                //productViewHolder.gridIcon.setImageResource();
 
                 //Used to get which item is clicked on in the view.
                 productViewHolder.bindData(new ViewHolderCallback(){
@@ -260,12 +308,14 @@ public class ProductPage extends AppCompatActivity {
         this.currentLocation = productIndex;
     }
 
-    /**Removes product from product list and database.*/
     public void removeProduct(int position){
         // removed product from products list and store in a temp var.
         Product tempProduct = products.remove(position);
+        //remove image associated with product
+        //removeFromFirebase(tempProduct.getImageUrl());
         // remove product using product id.
         myRef.child(tempProduct.productId).removeValue();
+        allProductRef.child(tempProduct.productId).removeValue();
         // update the adapter.
         productCardList.setAdapter(myAdapter);
     }
@@ -292,13 +342,98 @@ public class ProductPage extends AppCompatActivity {
                 saveOnClick();
             }
         });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Opens up our photos or Google drive
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent , 2);
+            }
+        });
+
+        addImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageUri != null){
+                    Toast.makeText(ProductPage.this, "You have selected an image", Toast.LENGTH_SHORT).show();
+                    uploadToFirebase(imageUri);
+                }else{
+                    Toast.makeText(ProductPage.this, "Please Select Image", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode ==2 && resultCode == RESULT_OK && data != null){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
 
+    private void uploadToFirebase(Uri uri){
 
+        final StorageReference fileRef = storageRef.child(System.currentTimeMillis() + "." + getFileExtension(uri));
+        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // store url to be used later.
+                        currentUrl = uri.toString();
+                        Toast.makeText(ProductPage.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                        imageUri = null;
+                    }
+                });
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                //progressBar.setVisibility(View.VISIBLE);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(ProductPage.this, "Uploading Failed !!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+    private String getFileExtension(Uri mUri){
 
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(mUri));
 
+    }
 
+    /**Remove image from firebase.*/
+    private void removeFromFirebase(String url){
+        if(url.length() == 0){
+            return;
+        }
+        StorageReference delRef = storageRef.child(url);
+        // Delete the file
+        delRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                // File deleted successfully
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Uh-oh, an error occurred!
+            }
+        });
+    }
 }
